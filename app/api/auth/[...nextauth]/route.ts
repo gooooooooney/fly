90632970fefe56bb70214480bde52b0653b58dba
张文光 <wenguangzhang@caixin.com>
2023-08-14 16:38:42 +0800

    chore: ignore env

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,10 +3,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
-<<<<<<< HEAD
 import EmailProvider from "next-auth/providers/email";
-=======
->>>>>>> 9ae5774a2b57fc69e3fe62199a6d2de3cfd05a72
 
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
@@ -14,17 +11,13 @@ export const authOptions: NextAuthOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-<<<<<<< HEAD
             httpOptions: {
                 timeout: 50000,
             },
-=======
->>>>>>> 9ae5774a2b57fc69e3fe62199a6d2de3cfd05a72
         }),
         GithubProvider({
             clientId: process.env.GITHUB_ID as string,
             clientSecret: process.env.GITHUB_SECRET as string,
-<<<<<<< HEAD
             httpOptions: {
                 timeout: 50000,
             },
@@ -33,8 +26,6 @@ export const authOptions: NextAuthOptions = {
             server: process.env.EMAIL_SERVER,
             from: process.env.EMAIL_FROM,
             //maxAge: 24 * 60 * 60, // 设置邮箱链接失效时间，默认24小时
-=======
->>>>>>> 9ae5774a2b57fc69e3fe62199a6d2de3cfd05a72
         }),
     ],
     secret: process.env.NEXTAUTH_SECRET as string,
