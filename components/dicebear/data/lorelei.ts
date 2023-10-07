ed06913c01005384c3446c47cff95df92cf66d92
张文光 <wenguangzhang@caixin.com>
2023-10-07 18:04:49 +0800

    feat: avatar

--- components/dicebear/data/lorelei.ts
+++ components/dicebear/data/lorelei.ts
@@ -23,19 +23,19 @@ export const seeds = [
 
 export const rotates = Array.from({ length: 36 }, (_, i) => ({
   label: `${i * 10}°`,
-  value: i * 10,
+  value: `${i * 10}`,
 }))
 
 // 生成一个数组在100到200之间, 每次增加10
 export const scales = Array.from({ length: 11 }, (_, i) => ({
   label: `${i * 10 + 100}%`,
-  value: i * 10 + 100,
+  value: `${i * 10 + 100}`,
 }))
 
 // 生成一个数组在0到50之间, 每次增加5
 export const radius = Array.from({ length: 11 }, (_, i) => ({
   label: `${i * 5}%`,
-  value: i * 5,
+  value: `${i * 5}`,
 }))
 
 // 生成10个背景颜色的数组, 用16进制表示, 颜色从冷色到暖色
@@ -60,7 +60,7 @@ export const backgroundType = [
 
 export const translateX = Array.from({ length: 20 }, (_, i) => ({
   label: `${i * 5}%`,
-  value: i * 5,
+  value: `${i * 5}`,
 }))
 
 export const hairs = Array.from({ length: 47 }, (_, i) => ({
--- components/dicebear/lorelei.tsx
+++ components/dicebear/lorelei.tsx
@@ -1,47 +1,113 @@
 import { Image, Input, Radio, RadioGroup, Select, SelectItem } from "@nextui-org/react";
 import { backgroundColor, backgroundType, hairs, radius as rdList, rotates, scales, seeds, translateX } from "./data/lorelei";
 import { useMemo, useState } from "react";
+import { assign, assignWith, isEmpty, isUndefined } from "lodash";
 
-export function Lorelei() {
+interface LoreleiProps {
+  seed: string
+  rotate: string
+  scale: string
+  hair: string
+  backgroundColor: string[]
+  translateX: string
+  radius: string
+  backgroundType: string
+  bgType: string
+}
 
-  const [seed, setSeed] = useState(new Set([seeds[0]])) 
-  const [rotate, setRotate] = useState(new Set([rotates[0].value])) 
-  const [scale, setScale] = useState(new Set([scales[0].value])) 
-  const [hair, setHair] = useState(new Set([hairs[0].value])) 
-  const [background, setBackground] = useState(new Set([backgroundColor[0]])) 
-  const [translate, setTranslate] = useState(new Set([translateX[0].value])) 
-  const [radius, setRadius] = useState(new Set([rdList[0].value])) 
-  const [bgType, setBgType] = useState(new Set([backgroundType[0].value])) 
-  const imgSrc= useMemo(() => {
-    return `https://api.dicebear.com/7.x/lorelei/svg?${
-     new URLSearchParams({
-      seed: [...seed].join(","),
-      rotate: [...rotate].join(","),
-      scale: [...scale].join(","),
-      hair: [...hair].join(","),
-      background: [...background].join(","),
-      translate: [...translate].join(","),
-      radius: [...radius].join(","),
-      bgType: [...bgType].join(","),
-     })
-    }`
-  }, [
-    seed,
-    rotate,
-    scale,
-    hair,
-    background,
-    translate,
-    radius,
-    bgType,
-  ])
+export function Lorelei(props: LoreleiProps) {
+
+  const propsInfo = Object.assign({
+    seed: seeds[0],
+    rotate: rotates[0].value,
+    scale: scales[0].value,
+    hair: hairs[0].value,
+    backgroundColor: [backgroundColor[0]],
+    translateX: translateX[0].value,
+    radius: rdList[0].value,
+    backgroundType: backgroundType[0].value,
+    bgType: backgroundType[0].value,
+  }, props)
+
+  // const [seed, setSeed] = useState(new Set([props.seed]))
+  // const [rotate, setRotate] = useState(new Set([props.rotate]))
+  // const [scale, setScale] = useState(new Set([props.scale]))
+  // const [hair, setHair] = useState(new Set([props.hair]))
+  // const [background, setBackground] = useState(new Set([props.backgroundColor]))
+  // const [translate, setTranslate] = useState(new Set([props.translateX]))
+  // const [radius, setRadius] = useState(new Set([props.radius]))
+  // const [bgType, setBgType] = useState(new Set([props.bgType]))
+  const getImgSrc = (propsParams: Partial<LoreleiProps>) => {
+    console.log(propsParams)
+    return `https://api.dicebear.com/7.x/lorelei/svg?${new URLSearchParams(assignWith({
+      seed: propsInfo.seed,
+      rotate: propsInfo.rotate,
+      scale: propsInfo.scale,
+      hair: propsInfo.hair,
+      backgroundColor: propsInfo.backgroundColor.join(","),
+      translateX: propsInfo.translateX,
+      radius: propsInfo.radius,
+      backgroundType: propsInfo.backgroundType,
+    }, {
+      ...propsParams,
+      backgroundColor: isEmpty(propsParams.backgroundColor) ? null : propsParams.backgroundColor!.join(",")
+    }, (objValue, srcValue) => {
+      return srcValue || objValue
+    }))
+      }`
+  }
+  const [imgSrc, setImgSrc] = useState(getImgSrc({}))
+
+  // const imgSrc = useMemo(() => {
+  //   return `https://api.dicebear.com/7.x/lorelei/svg?${new URLSearchParams({
+  //     seed: [...seed].join(","),
+  //     rotate: [...rotate].join(","),
+  //     scale: [...scale].join(","),
+  //     hair: [...hair].join(","),
+  //     backgroundColor: [...background].join(","),
+  //     translateX: [...translate].join(","),
+  //     radius: [...radius].join(","),
+  //     backgroundType: [...bgType].join(","),
+  //   })
+  //     }`
+  // }, [
+  //   seed,
+  //   rotate,
+  //   scale,
+  //   hair,
+  //   background,
+  //   translate,
+  //   radius,
+  //   bgType,
+  // ])
+  const getImgSrcParams = () => {
+    const params = new URL(imgSrc).searchParams
+    return [...params.entries()].reduce((acc, [key, value]) => {
+      if (key == "backgroundColor") {
+        acc[key] = value.split(",")
+      
+      } else {
+        acc[key] = value
+      }
+      return acc
+    }, {} as any)
+  }
   return (
-    <div>
-      <Image  width={100} height={100} src={imgSrc} ></Image>
+    <div className="flex flex-col gap-4">
+      <Image width={100} height={100} src={imgSrc} alt="avatar" ></Image>
       <section className="grid grid-cols-2 gap-4 ">
         <Select
-          onSelectionChange={e => setSeed(e)}
-          selectedKeys={seed}
+          onSelectionChange={e => {
+            if (typeof e !== "string") {
+              const src = getImgSrc({ 
+                ...getImgSrcParams(),
+                seed: [...e.values()][0] as string
+               })
+              console.log(src)
+              setImgSrc(src)
+            }
+          }}
+          defaultSelectedKeys={[propsInfo.seed]}
           label="Seed"
           placeholder="Select seed"
           className="max-w-xs"
@@ -53,8 +119,12 @@ export function Lorelei() {
           ))}
         </Select>
         <Select
-          onSelectionChange={setRotate}
-          selectedKeys={rotate}
+          onSelectionChange={e => {
+            if (typeof e !== "string") {
+              setImgSrc(getImgSrc({ rotate: [...e.values()][0] as string }))
+            }
+          }}
+          defaultSelectedKeys={[propsInfo.rotate]}
           label="Rotate"
           placeholder="Select rotate"
           className="max-w-xs"
@@ -66,8 +136,12 @@ export function Lorelei() {
           ))}
         </Select>
         <Select
-          onSelectionChange={setScale}
-          selectedKeys={scale}
+          onSelectionChange={e => {
+            if (typeof e !== "string") {
+              setImgSrc(getImgSrc({ scale: [...e.values()][0] as string }))
+            }
+          }}
+          defaultSelectedKeys={[propsInfo.scale]}
           label="Scale"
           placeholder="Select scale"
           className="max-w-xs"
@@ -79,8 +153,12 @@ export function Lorelei() {
           ))}
         </Select>
         <Select
-          onSelectionChange={setRadius}
-          selectedKeys={radius}
+          onSelectionChange={e => {
+            if (typeof e !== "string") {
+              setImgSrc(getImgSrc({ radius: [...e.values()][0] as string }))
+            }
+          }}
+          defaultSelectedKeys={[propsInfo.radius]}
           label="Radius"
           placeholder="Select radius"
           className="max-w-xs"
@@ -92,21 +170,34 @@ export function Lorelei() {
           ))}
         </Select>
         <Select
-          onSelectionChange={setBackground}
-          selectedKeys={background}
+          selectionMode="multiple"
+          onSelectionChange={e => {
+            if (typeof e !== "string") {
+              setImgSrc(getImgSrc({ backgroundColor: [...e.values()] as string[] }))
+            }
+          }}
+          defaultSelectedKeys={propsInfo.backgroundColor}
           label="Background color"
           placeholder="Select background color"
           className="max-w-xs"
         >
           {backgroundColor.map((bg) => (
-            <SelectItem key={bg} value={bg}>
+            <SelectItem startContent={
+              <span className="inline-block w-4 h-4 rounded-sm" style={{
+                backgroundColor: `#${bg}`
+              }}></span>
+            } key={bg} value={bg}>
               {bg}
             </SelectItem>
           ))}
         </Select>
         <Select
-          onSelectionChange={setBgType}
-          selectedKeys={bgType}
+          onSelectionChange={e => {
+            if (typeof e !== "string") {
+              setImgSrc(getImgSrc({ backgroundType: [...e.values()][0] as string }))
+            }
+          }}
+          defaultSelectedKeys={[propsInfo.bgType]}
           label="Background type"
           placeholder="Select background type"
           className="max-w-xs"
@@ -118,8 +209,12 @@ export function Lorelei() {
           ))}
         </Select>
         <Select
-          onSelectionChange={setTranslate}
-          selectedKeys={translate}
+          onSelectionChange={e => {
+            if (typeof e !== "string") {
+              setImgSrc(getImgSrc({ translateX: [...e.values()][0] as string }))
+            }
+          }}
+          defaultSelectedKeys={[propsInfo.translateX]}
           label="Translate X"
           placeholder="Select translate X"
           className="max-w-xs"
@@ -131,8 +226,12 @@ export function Lorelei() {
           ))}
         </Select>
         <Select
-          onSelectionChange={setHair}
-          selectedKeys={hair}
+          onSelectionChange={e => {
+            if (typeof e !== "string") {
+              setImgSrc(getImgSrc({ hair: [...e.values()][0] as string }))
+            }
+          }}
+          defaultSelectedKeys={[propsInfo.hair]}
           label="Hair"
           placeholder="Select hair type"
           className="max-w-xs"
