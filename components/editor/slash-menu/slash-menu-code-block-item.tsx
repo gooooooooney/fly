cb32f11ad7cfd407038cea9d3fe7557771854942
张文光 <wenguangzhang@caixin.com>
2023-10-12 17:15:44 +0800

    fix: type error

--- components/editor/slash-menu/slash-menu-code-block-item.tsx
+++ components/editor/slash-menu/slash-menu-code-block-item.tsx
@@ -6,9 +6,9 @@ import { insertOrUpdateBlock } from "../utils/insertOrUpdateBlock";
 export const SlashMenuCodeBlockItem: ReactSlashMenuItem<CustomBlockSchema> = {
   name: "codeBlock",
   execute: (editor) => {
-    insertOrUpdateBlock(editor, {
-      type: "codeBlock",
-    })
+    // insertOrUpdateBlock(editor, {
+    //   type: "codeBlock",
+    // })
   },
   icon: <Icons.DividerHorizontal size={18} />,
   group: "Basic blocks",
